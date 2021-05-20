7fdeb0136a4c6608a90f7c456043c555e7dddff3
christophersiem <christopher.siem@gmail.com>
2021-05-20 14:57:28 +0200

    Config husky

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import styled from "styled-components/macro";
-import { useEffect, useState } from "react";
-import githubApi from "./service/githubAPI";
+import styled from 'styled-components/macro'
+import { useEffect, useState } from 'react'
+import githubApi from './service/githubAPI'
 
 function App() {
-  const [profile, setProfile] = useState({});
-  const [error, setError] = useState("");
+  const [profile, setProfile] = useState({})
+  const [error, setError] = useState('')
 
   useEffect(() => {
     githubApi
-      .get("https://api.github.com/user")
-      .then((response) => response.data)
+      .get('https://api.github.com/user')
+      .then(response => response.data)
       .then(setProfile)
-      .catch((error) => setError(error.response.status));
-  }, []);
+      .catch(error => setError(error.response.status))
+  }, [])
 
   if (error) {
     return (
       <Page>
         <img src={`https://http.cat/${error}`} />
       </Page>
-    );
+    )
   }
 
   return (
@@ -27,10 +27,10 @@ function App() {
       <h1>Hallo, {profile.login} 👋🏽</h1>
       <Avatar src={profile.avatar_url} />
     </Page>
-  );
+  )
 }
 
-export default App;
+export default App
 
 const Page = styled.main`
   display: flex;
@@ -42,10 +42,10 @@ const Page = styled.main`
   left: 0;
   height: 100%;
   width: 100%;
-`;
+`
 
 const Avatar = styled.img`
   height: 200px;
   width: 200px;
   border-radius: 50%;
-`;
+`
--- frontend/src/service/githubAPI.js
+++ frontend/src/service/githubAPI.js
@@ -1,10 +1,10 @@
-import axios from "axios";
+import axios from 'axios'
 
-const token = process.env.REACT_APP_GITHUB_TOKEN;
+const token = process.env.REACT_APP_GITHUB_TOKEN
 
 const instance = axios.create({
   timeout: 1000,
   headers: { Authorization: `Bearer ${token}` },
-});
+})
 
-export default instance;
+export default instance
